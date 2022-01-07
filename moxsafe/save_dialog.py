ce661e2a1cd52d496d36967ce85092e42ca22615
Jerakin <the_rensvind@hotmail.com>
2022-01-07 12:57:45 +0100

    adding shortcuts to dialogs

--- moxsafe/save_dialog.py
+++ moxsafe/save_dialog.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
 from pathlib import Path
 
 
@@ -9,10 +9,14 @@ class SaveDialog(QtWidgets.QDialog):
         super(SaveDialog, self).__init__()
         uic.loadUi(Path(__file__).parent.parent / "res" / 'save_dialog.ui', self)
         self.ok_btn.clicked.connect(self.add)
-        self.ok_btn.clicked.connect(self.close)
+        self.cancel_btn.clicked.connect(self.close)
         self.show()
+        exit_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence("Esc"), self)
+        exit_shortcut.activated.connect(self.close)
+        accept_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence("Ctrl+Enter"), self)
+        accept_shortcut.activated.connect(self.add)
 
     def add(self):
         text = self.lineEdit.text()
-        if text:  # and "www.moxfield.com/decks/" in text:
+        if text:
             self.add_deck_signal.emit()
